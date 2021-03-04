07f57f0e4996d0f984374bec82d9ff8a4ac13399
HugoA44 <hugoaunette44@gmail.com>
2021-03-04 10:05:45 +0100

    administration

---
+++ migrations/Version20210301160918.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210301160918 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE infos_nutrition CHANGE fiber fiber INT NOT NULL, CHANGE protein protein INT NOT NULL, CHANGE sugar sugar INT NOT NULL, CHANGE salt salt INT NOT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE user');
+        $this->addSql('ALTER TABLE infos_nutrition CHANGE fiber fiber INT DEFAULT NULL, CHANGE protein protein INT DEFAULT NULL, CHANGE sugar sugar INT DEFAULT NULL, CHANGE salt salt INT DEFAULT NULL');
+    }
+}
--- src/Controller/admin/AdminController.php
+++ src/Controller/admin/AdminController.php
@@ -7,11 +7,16 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 use App\Entity\Product;
+use App\Entity\Ingredients;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+
+
 use Symfony\Component\HttpFoundation\Request;
 
 class AdminController extends AbstractController
@@ -37,17 +42,23 @@ class AdminController extends AbstractController
 
         // Création du formulaire
         $form = $this->createFormBuilder($product)
-        ->add('name', TextType::class, array('required' => true))
-        ->add('description', TextType::class, array('required' => true))
-        ->add('image', FileType::class, array('required' => true))
-        ->add('price', IntegerType::class, array('required' => true))
-        ->add('type', TextType::class, array('required' => true))
-        ->add('save', SubmitType::class, [
-            'label' => 'Add',   
-            'attr' => ['class' => 'btn-full']
+            ->add('name', TextType::class, array('required' => true))
+            ->add('description', TextType::class, array('required' => true))
+            ->add('image', FileType::class, array('required' => true))
+            ->add('price', IntegerType::class, array('required' => true))
+            ->add('type', ChoiceType::class, [
+                'choices' => [
+                    'Burger' => 'Burger',
+                    'Drink' => 'Drink',
+                    'Happy Meal' => 'Happy Meal',
+                ]
+            ])
+            ->add('save', SubmitType::class, [
+                'label' => 'Add',
+                'attr' => ['class' => 'btn-full']
 
-        ])
-        ->getForm();
+            ])
+            ->getForm();
 
         //Récupération des données
         $form->handleRequest($request);
@@ -74,20 +85,82 @@ class AdminController extends AbstractController
     }
 
     /**
+     * @Route("/admin/updateproduct/{id}", name="admin")
+     */
+    public function updateproduct(int $id, EntityManagerInterface $entityManager, Request $request): Response
+    {
+        //Récupération du manager de données
+        $entityManager = $this->getDoctrine()->getManager();
+
+        //Sélection d'une donnée par son id
+        $product = $entityManager->getRepository(Product::class)->find($id);
+
+        //Vérifictation si le produit existe bien
+        if (!$product) {
+            throw $this->createNotFoundException(
+                'No product found for id ' . $id
+            );
+        }
+
+        // Création du formulaire
+        $form = $this->createFormBuilder($product)
+            ->add('name', TextType::class, array('required' => true))
+            ->add('description', TextType::class, array('required' => true))
+            ->add('image', TextType::class, array('data_class' => null, 'required' => true))
+            ->add('price', IntegerType::class, array('required' => true))
+            ->add('type', ChoiceType::class, [
+                'choices' => [
+                    'Burger' => 'Burger',
+                    'Drink' => 'Drink',
+                    'Happy Meal' => 'Happy Meal',
+                ]
+            ])
+            ->add('save', SubmitType::class, [
+                'label' => 'Add',
+                'attr' => ['class' => 'btn-full']
+
+            ])
+            ->getForm();
+
+        //Récupération des données
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $file = $form->get('image')->getData();
+            //Récupération des données
+            $product = $form->getData();
+            /*             $product->setImage($file->getClientOriginalName()); */
+            //Sauvegardes des données
+            $entityManager->persist($product);
+            //Execution de l'insertion des données
+            $entityManager->flush();
+            //Sauvegarde du fichier
+            /*             $file->move('assets/img/products', $file->getClientOriginalName()); */
+        }
+
+
+        return $this->render('admin/addproduct.html.twig', [
+            'controller_name' => 'AdminController',
+            'form' => $form->createView(),
+            'action' => 'Add new product'
+        ]);
+    }
+
+    /**
      * @Route("/admin/removeproduct/{id}", name="remove_product")
      */
     public function removeProduct(int $id): Response
     {
         //Récupération du manager de données
         $entityManager = $this->getDoctrine()->getManager();
-        
+
         //Sélection d'une donnée par son id
         $product = $entityManager->getRepository(Product::class)->find($id);
 
         //Vérification si le produit existe bien
         if (!$product) {
             throw $this->createNotFoundException(
-                'Pas de produit trouvé avec cet id :'.$id
+                'Pas de produit trouvé avec cet id :' . $id
             );
         }
 
@@ -101,4 +174,104 @@ class AdminController extends AbstractController
         return $this->redirectToRoute('admin');
     }
 
+    /**
+     * @Route("/admin/addingredientinproduct/{id}", name="admin")
+     */
+    public function addIngredientInProduct(int $id, EntityManagerInterface $entityManager, Request $request): Response
+    {
+        //Sélection d'une donnée par son id
+        $product = $this->getDoctrine()->getRepository(Product::class)->find($id);
+
+        //Récupération du manager de données
+        $entityManager = $this->getDoctrine()->getManager();
+
+
+        //Vérifictation si le produit existe bien
+        if (!$product) {
+            throw $this->createNotFoundException(
+                'No product found for id ' . $id
+            );
+        }
+
+        // Création du formulaire
+        $form = $this->createFormBuilder($product)
+            ->add('ingredients', EntityType::class,
+                [
+                    'class' => Ingredients::class,
+                    'choice_label' => "name",
+                    'mapped' => false
+                ],
+                array('required' => true)
+            )
+
+            ->add('save', SubmitType::class, [
+                'label' => 'Add ingredient',
+                'attr' => ['class' => 'btn-full']
+
+            ])
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $product->addIngredient($form->get("ingredients")->getData());
+
+            $entityManager->flush();
+
+            return $this->redirectToRoute('admin');
+
+        }
+
+        return $this->render('admin/addingredientinproduct.html.twig', [
+            'controller_name' => 'AdminController',
+            'form' => $form->createView(),
+            'action' => 'Add new ingredient'
+        ]);
+    }
+
+
+     /**
+     * @Route("/admin/addingredient", name="admin")
+     */
+    public function addIngredient(EntityManagerInterface $entityManager, Request $request): Response
+    {
+        $ingredient = new Ingredients;
+
+        // Création du formulaire
+        $form = $this->createFormBuilder($ingredient)
+            ->add('name', TextType::class, array('required' => true))
+            ->add('image', FileType::class, array('required' => true))
+            ->add('save', SubmitType::class, [
+                'label' => 'Add',
+                'attr' => ['class' => 'btn-full']
+
+            ])
+            ->getForm();
+
+        //Récupération des données
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $file = $form->get('image')->getData();
+            //Récupération des données
+            $product = $form->getData();
+            $product->setImage($file->getClientOriginalName());
+            //Sauvegardes des données
+            $entityManager->persist($product);
+            //Execution de l'insertion des données
+            $entityManager->flush();
+            //Sauvegarde du fichier
+            $file->move('assets/img/ingredients', $file->getClientOriginalName());
+
+            return $this->redirectToRoute('admin');
+        }
+
+
+        return $this->render('admin/addingredient.html.twig', [
+            'controller_name' => 'AdminController',
+            'form' => $form->createView(),
+            'action' => 'Add new ingredient'
+        ]);
+    }
 }
